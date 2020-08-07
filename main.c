3e7f5f63aad0726bbf482f99da4cbd65836972e8
Daniel Hsu <d486250@gmail.com>
2020-08-07 14:05:41 +0800

    20200807 Copy

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "myPthread.h"
 
+typedef void (* CALLBACK) (int);
+
 int main()
 {
-    int ret;
-    char str[5];
-    ret = snprintf(str, 5, "ABCDEF");
-    printf("%s\n", str);
-    printf("%d\n", ret);
+    char * ptr;
+    //char ptr[4];
+    char * data = "123456789";
+    //ptr = calloc(sizeof(data), sizeof(char));
+    memcpy(ptr, data, 10);
+    printf("%s\n", ptr);
     return 0;
 }
